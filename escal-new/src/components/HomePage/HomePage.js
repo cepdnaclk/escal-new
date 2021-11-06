c579a1617b2a91dca80fac7b4b238b24a856c7e6
Dinura Dissanayake <dinuraransikadissanayake@gmail.com>
2021-11-06 14:26:09 +0530

    started homepage

--- escal-new/src/components/HomePage/HomePage.js
+++ escal-new/src/components/HomePage/HomePage.js
@@ -6,46 +6,75 @@ import TopNavbar from '../Navbar/Navbar';
 
 import tempImage from '../../images/Temp.jpg';
 
+const bodyStyle = {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '1.5rem',
+    overflow: 'hidden',
+    position: 'absoulte',
+}
+const carouselStyle = {
+    height: '80%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '50px',
+    position: 'relative',
+}
+const imgStyle = {
+    height: '80vh',
+    width: '100%',
+    objectFit: 'cover',
+}
+
 class HomePage extends React.Component{
     render(){
         return(
-            <div>
+            <div className={bodyStyle}>
             <TopNavbar/>
-            <Carousel fade>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={tempImage}
-                    alt="First slide"
-                    />
-                    <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    src={tempImage}
-                    alt="Second slide"
-                    />
+            <div className={carouselStyle}>
+                <Carousel fade>
+                    <Carousel.Item>
+                        <img className={imgStyle}
+                        src={tempImage}
+                        alt="First slide"
+                        />
+                        <Carousel.Caption>
+                        <h3>First slide</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className={imgStyle}
 
-                    <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    src={tempImage}
-                    alt="Third slide"
-                    />
+                        src={tempImage}
+                        alt="Second slide"
+                        />
 
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                </Carousel>
+                        <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className={imgStyle}
+                        src={tempImage}
+                        alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    </Carousel>
+            </div>
             </div>
         );
     }
