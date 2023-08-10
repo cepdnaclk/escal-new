00f4527caf38bac2b36791a43122d303e7f4bf5e
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-08-10 19:49:26 +0530

    Add all 3yp projects to _data

--- python_scripts/extract_embedded_system_projects.py
+++ python_scripts/extract_embedded_system_projects.py
@@ -1,7 +1,6 @@
 import requests
 import json
-
-
+import os
 
 def fetch_and_append_data(category_id, batch_id, output_filename):
     try:
@@ -10,18 +9,20 @@ def fetch_and_append_data(category_id, batch_id, output_filename):
 
         if response.status_code == 200:
             data = response.json()
+
             try:
                 with open(output_filename, 'r') as json_file:
                     existing_data = json.load(json_file)
             except FileNotFoundError:
                 existing_data = []
 
-            existing_data.append(data)
-
             if data not in existing_data:
+                existing_data.append(data)
                 with open(output_filename, 'w') as json_file:
                     json.dump(existing_data, json_file, indent=4)
                 print(f"Data fetched and appended to {output_filename}")
+            else:
+                print("Data is already present in the JSON file.")
         else:
             print(f"Failed to fetch data. Status code: {response.status_code}")
     except requests.exceptions.RequestException as e:
@@ -29,7 +30,11 @@ def fetch_and_append_data(category_id, batch_id, output_filename):
 
 if __name__ == '__main__':
     category_id = "3yp"
-    batch_ids = ["E17","E16", "E15", "E14"]
-    output_filename = "output_data.json"
+    batch_ids = ["E17", "E16", "E15", "E14"]
+    output_directory = "D:\ESCAL\escal-new\_data"
+    os.makedirs(output_directory, exist_ok=True)  # Create the output directory if it doesn't exist
+    output_filename = os.path.join(output_directory, "output.json")
+
+    # Fetch and append data for each batch ID
     for batch_id in batch_ids:
         fetch_and_append_data(category_id, batch_id, output_filename)
